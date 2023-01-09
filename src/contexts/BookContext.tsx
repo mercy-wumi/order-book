455abf02c6352d46c22e581d9e9afff3b9bb2acc
mercy-wumi <m.adams1909@gmail.com>
2023-01-09 23:03:53 +0100

    refactor

--- src/contexts/BookContext.tsx
+++ src/contexts/BookContext.tsx
@@ -12,7 +12,7 @@ const BookContextProvider: React.FC<Props> = ({ children }) => {
     })
     console.log(manager)
     useEffect(() => {
-        const ws = new WebSocket('wss://comx-sand-api.afexnigeria.com/stream/trades')
+        const ws = new WebSocket('wss://comx-sand-api.afexnigeria.com/stream/trades?authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxNjAsInVzZXJuYW1lIjoic2EuYWJkdWxnYWZhckBnbWFpbC5jb20iLCJleHAiOjE2NDAwODUxODIsImVtYWlsIjoic2EuYWJkdWxnYWZhckBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTYzOTk5ODc4Mn0.1gAY5vceMv12RfwqlaL_Msyk7wJH0U4BtTGsKh2IfCQ')
         ws.onmessage = (event) => {
             const json = JSON.parse(event.data)
             try {
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -7,21 +7,7 @@ import { BookType, IBooks } from '../@types/book'
 
 const Dashboard = () => {
     const { orderBook, setOrderBook } = useContext(BookContext) as BookType
-    useEffect(() => {
-        const ws = new WebSocket('wss://comx-sand-api.afexnigeria.com/stream/trades?authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxNjAsInVzZXJuYW1lIjoic2EuYWJkdWxnYWZhckBnbWFpbC5jb20iLCJleHAiOjE2NDAwODUxODIsImVtYWlsIjoic2EuYWJkdWxnYWZhckBnbWFpbC5jb20iLCJvcmlnX2lhdCI6MTYzOTk5ODc4Mn0.1gAY5vceMv12RfwqlaL_Msyk7wJH0U4BtTGsKh2IfCQ')
-        ws.onmessage = (event) => {
-            const json = JSON.parse(event.data)
-            try {
-                if (json) {
-                    setOrderBook(json)
-                    console.log(orderBook)
-                }
-            }
-            catch (err) {
-                console.log(err)
-            }
-        }
-    }, [])
+
     return (
         <div className='bg-backgroundColor'>
             <Navbar />
